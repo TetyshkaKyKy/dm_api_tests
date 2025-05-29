6abea71d22135bf52b72e86152e0c7e13ee452f4
tetyshkakyky <tetyshkakyky@yandex.ru>
2025-05-29 15:07:43 +0300

    Добавлена валидация ответов с помощью библиотеки pyhamcrest

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from hamcrest import assert_that, all_of, has_property, has_properties, starts_with, instance_of, equal_to
+
+
 def test_post_v1_account_login(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
@@ -5,5 +10,26 @@ def test_post_v1_account_login(account_helper, prepare_user):
 
     account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.activate_user_email(login=login)
-    user_login = account_helper.user_login(login=login, password=password)
-    assert user_login.status_code == 200, 'Пользователь не смог авторизоваться'
+    user_login = account_helper.user_login(login=login, password=password, validate_response=True,
+                                           validate_headers=False)
+
+    assert_that(
+        user_login,
+        has_property(
+            'resource', all_of(
+                has_property('login', starts_with('ek-n-pavlova')),
+                has_property('registration', instance_of(datetime)),
+                has_properties(
+                    {
+                        'rating': has_properties(
+                            {
+                                'enabled': equal_to(True),
+                                'quality': equal_to(0),
+                                'quantity': equal_to(0)
+                            }
+                        )
+                    }
+                ),
+            )
+        )
+    )
